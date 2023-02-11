a156ef664e97e6b0c000bf0c9f06c2e3b92a1b01
arif-uddin-2k <123826264+arif-uddin-2k@users.noreply.github.com>
2023-02-11 17:42:33 +0000

    Message

---
+++ Lab-03-practice/Student.java
@@ -0,0 +1,26 @@
+//creating a class "Student"
+public class Student {
+
+    //declare instance variables 
+    String name;
+    int age;
+    double cgpa;
+
+    //declare method with no parameter
+    void displayInfo() {
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("CGPA : " + cgpa);  
+    }
+    public static void main(String[] args){
+
+        //creating an "Student" object
+        Student obj = new Student();
+        obj.name = "Arif";
+        obj.age = 22;
+        obj.cgpa = 3.42;
+        obj.displayInfo();
+    }
+}
+        
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CSE-215-oop-by-java
+Name : Khandaker Arif Uddin
+Section : K
+Stu. Id : 221-15-5017
